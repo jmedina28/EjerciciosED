5454812177ada05b3fc56309b97f1f25bf94df0f
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-04-14 14:37:32 +0200

    funciona con una cadena no inputeable

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,17 +1,22 @@
 class Modelo():
-    def __init__(self, texto):
-        self.texto = texto
+    def __init__(self):
+        self.texto = "Hola, me llamo Juan Antonio y necesito que me devuelvas este texto en mayúsculas."
     
 class Vista():
     def __init__(self):
         self.modelo = Modelo()
     
-    def imprimir(self):
-        print(self.modelo.texto)
+    def mayusculas(self):
+        print(self.modelo.texto.upper())
         
 class Controlador():
     def __init__(self):
         self.vista = Vista()
     
-    def imprimir(self):
-        self.vista.imprimir()
+    def get_textomayusculas(self):
+        self.vista.mayusculas()
+        
+class Cliente():
+    controlador = Controlador()
+    controlador.get_textomayusculas()
+Cliente()
